e9e3140b97fa9f919fee8d081ff2698696a1bd54
Juraj Begovac <juraj.begovac@gmail.com>
2017-11-14 09:35:47 +0100

    [Tests] Add more tests

--- rxfeedback/src/test/kotlin/org/notests/rxfeedback/RxFeedbackObservableTests.kt
+++ rxfeedback/src/test/kotlin/org/notests/rxfeedback/RxFeedbackObservableTests.kt
@@ -3,8 +3,9 @@ package org.notests.rxfeedback
 import io.reactivex.Observable
 import io.reactivex.schedulers.TestScheduler
 import org.junit.Assert.assertEquals
-import org.junit.Before
+import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.notests.rxtestutils.Event
 import org.notests.rxtestutils.complete
 import org.notests.rxtestutils.next
 import org.notests.rxtestutils.start
@@ -16,11 +17,6 @@ import java.util.concurrent.TimeUnit
 class RxFeedbackObservableTests {
 
     var scheduler = TestScheduler()
-
-    @Before
-    fun setup() {
-
-    }
 
     // todo add testEventsAreArrivingOnCorrectScheduler - haven't investigated SerialDispatchQueueScheduler
 
@@ -42,7 +38,7 @@ class RxFeedbackObservableTests {
     @Test
     fun testImmediateFeedbackLoop() {
         val res = scheduler.start {
-            val feedback: Feedback<String, String> = {
+            val feedbackLoop: Feedback<String, String> = {
                 it.source.flatMap {
                     when (it) {
                         "initial" -> Observable.just("_a")
@@ -57,7 +53,7 @@ class RxFeedbackObservableTests {
             Observables.system("initial",
                     { oldState, append: String -> oldState + append },
                     scheduler,
-                    listOf(feedback)
+                    listOf(feedbackLoop)
             )
         }
 
@@ -72,7 +68,7 @@ class RxFeedbackObservableTests {
     @Test
     fun testImmediateFeedbackLoopParallel() {
         val res = scheduler.start {
-            val feedback: (ObservableSchedulerContext<String>) -> Observable<String> = {
+            val feedbackLoop: (ObservableSchedulerContext<String>) -> Observable<String> = {
                 it.source.flatMap {
                     when (it) {
                         "initial" -> return@flatMap Observable.just("_a")
@@ -88,7 +84,7 @@ class RxFeedbackObservableTests {
             Observables.system("initial",
                     { oldState, append: String -> oldState + append },
                     scheduler,
-                    listOf(feedback, feedback, feedback)
+                    listOf(feedbackLoop, feedbackLoop, feedbackLoop)
             )
         }
 
@@ -152,6 +148,65 @@ class RxFeedbackObservableTests {
 
     @Test
     fun testImmediateFeedbackLoopParallel_react_set() {
-        // todo in progress..
+        val res = scheduler.start {
+            val feedbackLoop: (ObservableSchedulerContext<String>) -> Observable<String> =
+                    reactSet<String, String, String>(
+                            query = { it.needsToAppendParallel },
+                            effects = {
+                                Observable.just(it)
+                            })
+
+            Observables.system("initial",
+                    { oldState, append: String -> oldState + append },
+                    scheduler,
+                    listOf(feedbackLoop, feedbackLoop, feedbackLoop)
+            )
+        }
+
+        assertTrue((res.events()[1].value as Event.Next<String>).value.contains("_a") ||
+                (res.events()[1].value as Event.Next<String>).value.contains("_b"))
+        assertTrue((res.events()[2].value as Event.Next<String>).value.contains("_a") ||
+                (res.events()[2].value as Event.Next<String>).value.contains("_b"))
+        assertTrue((res.events()[1].value as Event.Next<String>).value.contains("_a") ||
+                (res.events()[2].value as Event.Next<String>).value.contains("_a"))
+        assertTrue((res.events()[1].value as Event.Next<String>).value.contains("_b") ||
+                (res.events()[2].value as Event.Next<String>).value.contains("_b"))
+
+        var ignoringAB = res.events().map {
+            var value = (it.value as Event.Next<String>).value
+            value = value.replace("_a", "_x")
+            value = value.replace("_b", "_x")
+            return@map value
+        }
+
+        assertEquals(listOf("initial", "initial_x", "initial_x_x", "initial_x_x_c", "initial_x_x_c_c", "initial_x_x_c_c_c"),
+                ignoringAB)
+    }
+
+    @Test
+    fun testUIBindFeedbackLoopReentrancy() {
+        val res = scheduler.start {
+            Observables.system(initialState = "initial",
+                    reduce = { oldState, append: String -> oldState + append },
+                    scheduler = scheduler,
+                    scheduledFeedback = listOf(bind {
+                        val results = it.source.flatMap {
+                            when (it) {
+                                "initial" -> Observable.just("_a")
+                                "initial_a" -> Observable.just("_b")
+                                "initial_a_b" -> Observable.just("_c")
+                                else -> Observable.never<String>()
+                            }
+                        }
+                        return@bind Bindings(emptyList(), listOf(results))
+                    }))
+        }
+
+        assertEquals(listOf(
+                next(200, "initial"),
+                next(200, "initial_a"),
+                next(200, "initial_a_b"),
+                next(200, "initial_a_b_c")
+        ), res.events())
     }
 }
