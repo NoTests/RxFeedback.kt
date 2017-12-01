f575ea2b53d20e6cdb362c6e37d36f1c0c3393e0
Juraj Begovac <juraj.begovac@gmail.com>
2017-12-01 20:21:21 +0100

    [Examples] Counter

---
+++ app/src/main/kotlin/org/notests/rxfeedbackexample/Counter.kt
@@ -0,0 +1,59 @@
+package org.notests.rxfeedbackexample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jakewharton.rxbinding2.view.RxView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.Disposables
+import kotlinx.android.synthetic.main.activity_counter.*
+import org.notests.rxfeedback.Bindings
+import org.notests.rxfeedback.Observables
+import org.notests.rxfeedback.bind
+import org.notests.rxfeedback.system
+
+/**
+ * Created by Juraj Begovac on 01/12/2017.
+ */
+
+typealias State = Int
+
+enum class Event {
+    increment, decrement
+}
+
+class Counter : AppCompatActivity() {
+
+    var disposable: Disposable = Disposables.empty()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_counter)
+
+        disposable = Observables.system(initialState = 0,
+                reduce = { state, event: Event ->
+                    when (event) {
+                        Event.increment -> state + 1
+                        Event.decrement -> state - 1
+                    }
+                },
+                scheduler = AndroidSchedulers.mainThread(),
+                scheduledFeedback = listOf(
+                        bind {
+                            val subscriptions = listOf(
+                                    it.source.map { it.toString() }.subscribe { label.text = it }
+                            )
+                            val events = listOf(
+                                    RxView.clicks(plus).map { Event.increment },
+                                    RxView.clicks(minus).map { Event.decrement }
+                            )
+                            return@bind Bindings(subscriptions, events)
+                        }))
+                .subscribe()
+    }
+
+    override fun onDestroy() {
+        disposable.dispose()
+        super.onDestroy()
+    }
+}
